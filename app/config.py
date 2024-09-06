0acfb189a162711445d303ebafbbb4f51adae83d
Jerry Lin <vp780412@gmail.com>
2024-09-06 16:58:30 +0800

    feat: add Docker support and configure database settings
    - Add a new Dockerfile
    - Update DATABASE_URL in config.py from test database to sql_app database
    - Refactor database.py to use the new DATABASE_URL from config
    - Modify main.py to run the app directly instead of specifying the module path
    - Add a new docker-compose.yml file
    
    Signed-off-by: Jerry Lin <vp780412@gmail.com>

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     ENV = os.getenv("ENV", "development")
     DEBUG = os.getenv("DEBUG", "True") == "True"
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
